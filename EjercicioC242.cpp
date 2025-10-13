2d581730532ae5a5a963d3369af0dd2d6d7f4750
Caguanin <lcaguana@alumno.montepiedra.edu.ec>
2025-10-13 08:38:00 -0500

    Add files via upload

---
+++ EjercicioC242.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // EjercicioC242: Imprimir numeros del 1 al 10
+    for (int i = 1; i <= 10; ++i) cout << i << " ";
+    cout << endl;
+    return 0;
+}
